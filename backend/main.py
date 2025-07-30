0cb11c3e753bbe2d29320fde3024b5204929304b
João Pedro Cordeiro Guimarães <jpedrocguimaraes@hotmail.com>
2025-07-30 16:08:47 -0300

    .

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,10 @@ from typing import List, Optional
 import os
 
 # Database setup
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////home/ec2-user/dbdata/tasks.db")
+if os.getenv("GITHUB_ACTIONS") == "true":
+    DATABASE_URL = "sqlite:///./test.db"
+else:
+    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////home/ec2-user/dbdata/tasks.db")
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
